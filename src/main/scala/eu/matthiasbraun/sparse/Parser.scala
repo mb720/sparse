f57220f6a1a8fa91f0abedf8994f5db8bcef2d54
Matthias Braun <matthias.braun@liwest.at>
2015-07-20 00:55:23 +0200

    Correct errors in documentation. Small cleanups.

--- src/main/scala/eu/matthiasbraun/sparse/Parser.scala
+++ src/main/scala/eu/matthiasbraun/sparse/Parser.scala
@@ -1,12 +1,12 @@
 package eu.matthiasbraun.sparse
-import java.io.File
+
 import resource._
 import scala.collection.mutable.ArrayBuffer
-import scala.Vector
 import scala.io.Source
 import scala.util.Try
 import scala.util.Failure
 import scala.util.Success
+
 /**
  * A parser for extracting text blocks from a source such as a file.
  * Created by Matthias Braun on 5/7/15.
@@ -16,7 +16,7 @@ object Parser {
   /** Marks the start or end of a text block. The `linePredicate` and its `offset` define where in the text this marker is placed. */
   case class BlockMarker(linePredicate: ((String, Int) => Boolean), offset: Int = 0)
 
-  /** Thrown when the text couldn't be properly parsed.*/
+  /** Thrown when the text couldn't be properly parsed. */
   case class SparseException(msg: String) extends RuntimeException(msg)
 
   /** A bunch of lines are a text block. */
@@ -33,94 +33,97 @@ object Parser {
    */
   trait MarkerFactory {
     def apply(line: String): BlockMarker = apply(line.equals _)
+
     def apply(linePredicate: (String => Boolean)): BlockMarker = apply((line, lineNr) => linePredicate(line))
+
     def apply(lineAndLineNrPredicate: ((String, Int) => Boolean)): BlockMarker
   }
 
   /**
-   * When a line within the parsed text matches the `predicate`, we set a [[BlockMarker]] marking the beginning of a [[TextBlock]] at that line.
+   * When a line within the parsed text matches the `predicate`, we set a [[eu.matthiasbraun.sparse.Parser.BlockMarker]] at that line.
+   * This marker indicates the start of a [[eu.matthiasbraun.sparse.Parser.TextBlock]].
    * Example usage:
    *
-   *  `parse(source, from("start line"), to("end line"))
-   *  `parse(source, from(_.endsWith("start line suffix")), to(_.startsWith("end line prefix")))
-   *  `parse(source, from((line, lineNr) => line == "start line" && lineNr > 10 ), to("end line))
+   * `parse(source, from("start line"), to("end line"))`
+   * `parse(source, from(_.endsWith("start line suffix")), to(_.startsWith("end line prefix")))`
+   * `parse(source, from((line, lineNr) => line == "start line" && lineNr > 10 ), to("end line))`
    */
   object from extends MarkerFactory {
-    override def apply(predicate: ((String, Int) => Boolean)) = {
-      BlockMarker(predicate)
-    }
-  }
-  /**
-   * When a line within the parsed text matches the `predicate`, we set a [[BlockMarker]] at that line. This marker indicates the end of a [[TextBlock]].
-   * Example usage:
-   *
-   *  `parse(source, from("start line"), to("end line"))
-   *  `parse(source, from(_.endsWith("start line suffix")), to(_.startsWith("end line prefix")))
-   *  `parse(source, from("start line), to((line, lineNr) => line == "end line" && lineNr > 10 ))
-   */
-  object to extends MarkerFactory {
-    /* "to" is technically the same as "from". It exists so the user can call `parse(file, from("start"), to("end"))` */
-    override def apply(predicate: ((String, Int) => Boolean)) = {
-      BlockMarker(predicate)
-    }
-  }
-  /**
-   * When a line within the parsed text matches the `predicate`, we set a [[BlockMarker]] after that line. This marker may either indicate the start or end of a [[TextBlock]].
-   * Example usage:
-   *
-   *  `parse(source, after("line before start"), to("end line"))
-   *  `parse(source, from("start line), after(_.startsWith("prefix of line before end")))
-   *  `parse(source, after((line, lineNr) => line == "line before start" && lineNr > 10), to("end line"))
-   */
-  object after extends MarkerFactory {
-    override def apply(predicate: ((String, Int) => Boolean)) = {
-      BlockMarker(predicate, offset = +1)
-    }
-  }
-  /**
-   * When a line within the parsed text matches the `predicate`, we set a [[BlockMarker]] before that line. This marker indicates the start of a [[TextBlock]].
-   * Example usage:
-   *
-   *  `parse(source, before("line after start"), to("end line"))
-   *  `parse(source, before(_.startsWith("prefix of line after start"), to("end line"))
-   *  `parse(source, before((line, lineNr) => line == "line after start" && lineNr > 10), to("end line"))
-   */
-  object before extends MarkerFactory {
-    override def apply(predicate: ((String, Int) => Boolean)) = {
-      BlockMarker(predicate, offset = -1)
-    }
-  }
-  /**
-   * When a line within the parsed text matches the `predicate`, we set a [[BlockMarker]] before that line. This marker indicates the end of a [[TextBlock]].
-   * Example usage:
-   *
-   *  `parse(source, from("start line"), until("line after end"))
-   *  `parse(source, from("start line"), until(_.startsWith("prefix of line after end")))
-   *  `parse(source, from("start line), until((line, lineNr) => line == "line after end " && lineNr > 10 ))
-   */
-  object until extends MarkerFactory {
-    /* "until" is the same as "before". It exists so the user can call `parse(file, before("start"), until("end"))` */
-    override def apply(predicate: ((String, Int) => Boolean)) = {
-      BlockMarker(predicate, offset = -1)
-    }
+    override def apply(predicate: ((String, Int) => Boolean)) = BlockMarker(predicate)
   }
 
   /**
-   * Parses [[eu.matthiasbraun.sparse.TextBlock]]s from a `source`, such as a file. The start and end of a text block are defined by the `start` and `end` [[eu.matthiasbraun.sparse.BlockMarker]]s.
-   * These markers match lines within the text. The caller can create those markers using [[eu.matthiasbraun.sparse.from]] or [[eu.matthiasbraun.sparse.until]], for example.
+   * When a line within the parsed text matches the `predicate`, we set a [[eu.matthiasbraun.sparse.Parser.BlockMarker]] at that line.
+   * This marker indicates the end of a [[eu.matthiasbraun.sparse.Parser.TextBlock]].
+   * Example usage:
+   *
+   * `parse(source, from("start line"), to("end line"))`
+   * `parse(source, from(_.endsWith("start line suffix")), to(_.startsWith("end line prefix")))`
+   * `parse(source, from("start line), to((line, lineNr) => line == "end line" && lineNr > 10 ))`
+   */
+  object to extends MarkerFactory {
+    /* "to" is technically the same as "from". It exists so the user can call `parse(file, from("start"), to("end"))` */
+    override def apply(predicate: ((String, Int) => Boolean)) = BlockMarker(predicate)
+  }
+
+  /**
+   * When a line within the parsed text matches the `predicate`, we set a [[eu.matthiasbraun.sparse.Parser.BlockMarker]] after that line.
+   * This marker may either indicate the start or end of a [[eu.matthiasbraun.sparse.Parser.TextBlock]].
+   * Example usage:
+   *
+   * `parse(source, after("line before start"), to("end line"))
+   * `parse(source, from("start line), after(_.startsWith("prefix of line before end")))
+   * `parse(source, after((line, lineNr) => line == "line before start" && lineNr > 10), to("end line"))`
+   */
+  object after extends MarkerFactory {
+    override def apply(predicate: ((String, Int) => Boolean)) = BlockMarker(predicate, offset = +1)
+  }
+
+  /**
+   * When a line within the parsed text matches the `predicate`, we set a [[eu.matthiasbraun.sparse.Parser.BlockMarker]] before that line.
+   * This marker indicates the start of a [[eu.matthiasbraun.sparse.Parser.TextBlock]].
+   * Example usage:
+   *
+   * `parse(source, before("line after start"), to("end line"))
+   * `parse(source, before(_.startsWith("prefix of line after start"), to("end line"))
+   * `parse(source, before((line, lineNr) => line == "line after start" && lineNr > 10), to("end line"))`
+   */
+  object before extends MarkerFactory {
+    override def apply(predicate: ((String, Int) => Boolean)) = BlockMarker(predicate, offset = -1)
+  }
+
+  /**
+   * When a line within the parsed text matches the `predicate`, we set a [[eu.matthiasbraun.sparse.Parser.BlockMarker]] before that line.
+   * This marker indicates the end of a [[eu.matthiasbraun.sparse.Parser.TextBlock]].
+   * Example usage:
+   *
+   * `parse(source, from("start line"), until("line after end"))
+   * `parse(source, from("start line"), until(_.startsWith("prefix of line after end")))
+   * `parse(source, from("start line), until((line, lineNr) => line == "line after end " && lineNr > 10 ))`
+   */
+  object until extends MarkerFactory {
+    /* "until" is the same as "before". It exists so the user can call `parse(file, before("start"), until("end"))` */
+    override def apply(predicate: ((String, Int) => Boolean)) =  BlockMarker(predicate, offset = -1)
+  }
+
+  /**
+   * Parses [[eu.matthiasbraun.sparse.Parser.TextBlock]]s from a `source`, such as a file.
+   * The start and end of a text block are defined by the `start` and `end` [[eu.matthiasbraun.sparse.Parser.BlockMarker]]s.
+   * These markers match lines within the text.
+   * The caller can create those markers using [[eu.matthiasbraun.sparse.Parser.from]] or [[eu.matthiasbraun.sparse.Parser.until]], for example.
    * Example usage:
    * {{{
    * val file = new File("parse/this/file")
-   * val blocksMaybe = parse(fromFile(file), from("startline"), to("endline"))
+   * val blocksMaybe = parse(fromFile(file), from("start line"), to("end line"))
    *   blocksMaybe match {
    *     case Success(blocks)    => blocks.foreach { println }
    *     case Failure(exception) => println(exception)
    * }
    * }}}
-   * @param source the [[Source]] that contains the text that we want to parse
-   * @param start the text blocks we want to parse begin at this [[BlockMarker]]
-   * @param end the text blocks we want to parse stop at this [[BlockMarker]]
-   * @return either a list of [[TextBlock]]s wrapped in a [[Success]] or a [[Failure]] with the first exception that occurred during parsing.
+   * @param source the [[scala.io.Source]] that contains the text that we want to parse
+   * @param start the text blocks we want to parse begin at this [[eu.matthiasbraun.sparse.Parser.BlockMarker]]
+   * @param end the text blocks we want to parse stop at this [[eu.matthiasbraun.sparse.Parser.BlockMarker]]
+   * @return either a list of [[eu.matthiasbraun.sparse.Parser.TextBlock]]s wrapped in a [[scala.util.Success]] or a [[scala.util.Failure]] with the first exception that occurred during parsing.
    */
   def parse(source: Source, start: BlockMarker, end: BlockMarker): Try[List[TextBlock]] = {
 
@@ -136,7 +139,7 @@ object Parser {
       // Appending to ArrayBuffers is O(1)
       val indices = ArrayBuffer[Int]()
       // We expect the first line to be matched a starting line
-      var startExpected = true;
+      var startExpected = true
 
       for ((line, lineNr) <- lineStream.zipWithIndex) {
 
@@ -168,8 +171,8 @@ object Parser {
         getStartAndEndPairs(lineStream).map(pair => getBlock(pair, lineStream))
     }
     blocksMaybe.either match {
-      case Left(exceptions) => Failure(exceptions(0))
-      case Right(blocks)    => Success(blocks)
+      case Left(exceptions) => Failure(exceptions.head)
+      case Right(blocks) => Success(blocks)
     }
   }
 }
